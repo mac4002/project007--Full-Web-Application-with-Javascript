ebacda6a8f0c45a7e7bccac57ee3cfb0a9d20988
Mac O <omenemac@outlook.com>
2018-06-04 12:29:12 +0200

    o

--- public/js/tchat-client.js
+++ public/js/tchat-client.js
@@ -1,2 +1,8 @@
 console.log('Ouverte du tchat cote client');
 let socket = io();
+
+let formConnection = document.getElementById('form-connection');
+formConnection.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Authentification');
+});
